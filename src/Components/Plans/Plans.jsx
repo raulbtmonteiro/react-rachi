fead452050371756ef66759aa6c9fdaf2dda9b6c
Raul Monteiro <raulbtmonteiro@gmail.com>
2022-05-28 10:32:21 -0300

    rev25

--- src/Components/Plans/Plans.jsx
+++ src/Components/Plans/Plans.jsx
@@ -6,16 +6,19 @@ function PlanCard () {
   const plans = [
     {
       title: 'Bronze',
+      isFavorite: false,
       price: '28',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.'
     },
     {
       title: 'Prata',
+      isFavorite: true,
       price: '57',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.'
     },
     {
       title: 'Ouro',
+      isFavorite: false,
       price: '94',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.'
     }
@@ -23,8 +26,9 @@ function PlanCard () {
 
   return (
     plans.map( (item) => (
-      <div key={item.title} className='plan-card'>
+      <div key={item.title} className={item.isFavorite ? 'plan-card-favorite' : 'plan-card'}>
         <h3 className='plan-card-title'>{item.title}</h3>
+        <button className={item.isFavorite ? 'isfavorite-show' : 'isfavorite-notshow'}>Preferido</button>
         <h4 className='plan-card-price'><p className='plan-card-sifrao'>R$</p>{item.price}<p className='plan-card-month'>/mês</p></h4>
         <p className='plan-card-description'>{item.description}</p>
         <button className='plan-card-button'>Assinar</button>
